cafe3aeef5946896ef471fd6c1549f20e905e8ff
eliox01 <espejox01@gmail.com>
2020-03-05 02:27:10 +0000

    added print functions

---
+++ main.py
@@ -0,0 +1,24 @@
+print ('hello world !')
+print ('that was easy')
+print (1 + 2)
+print ( "whatever")
+print ( "one" + " two" )
+print ('Kulvir stated "he is the best pingpong player"')
+
+#name = input('Please enter your name: ')
+#print('Hello ! ' + name + ' Nice to meet you !')
+#print(type(name))
+
+
+print ('Blank line\n in the middle of string')
+
+sentence = 'Thank you for calling Service Desk'
+
+print(sentence.upper())
+print(sentence)
+print(sentence.lower())
+print(sentence.capitalize())
+print(sentence.count('a'))
+print(len(sentence))
+ 
+
